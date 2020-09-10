8f387ca481ed667aaacd248c9094fa21dadb405c
Hari Bhandari <2012bhandari.ha@gmail.com>
2020-09-10 17:41:47 +0100

    added react-loadable libary

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
-
 import React, {Fragment} from 'react';
-import WelcomePage from "./Components/sections/Welcome /WelcomePage";
-import Navbar from "./Components/layout/Navbar";
-import AboutMe from "./Components/sections/About Me/AboutMe";
-import Projects from "./Components/sections/Projects/Projects";
-import ContactMe from "./Components/sections/About Me/ContactMe";
-import Skills from "./Components/sections/About Me/Skills";
-import Footer from "./Components/layout/Footer";
+import loadable from 'react-loadable-visibility/loadable-components';
+const WelcomePage=loadable(()=>import("./Components/sections/Welcome /WelcomePage"),{ssr:true})
+const Navbar=loadable(()=>import("./Components/layout/Navbar"),{ssr:true})
+const AboutMe=loadable(()=>import("./Components/sections/About Me/AboutMe"),{ssr:true})
+const Projects=loadable(()=>import("./Components/sections/Projects/Projects"),{ssr:true})
+const ContactMe=loadable(()=>import("./Components/sections/About Me/ContactMe"),{ssr:true})
+const Skills=loadable(()=>import("./Components/sections/About Me/Skills"),{ssr:true})
+const Footer=loadable(()=>import("./Components/layout/Footer"),{ssr:true})
 
 function App() {
 
