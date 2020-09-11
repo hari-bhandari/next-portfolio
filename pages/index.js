e6d8e2e6e19c4147e537df462187c4b981e1c08a
Hari Bhandari <2012bhandari.ha@gmail.com>
2020-09-11 22:38:24 +0100

    test1

--- pages/index.js
+++ pages/index.js
@@ -6,43 +6,43 @@ const Navbar = LoadableVisibility({
     loader: () => import("./Components/layout/Navbar"),
     loading: Loader
 });
-const WelcomePage = LoadableVisibility({
-    loader: () => import("./Components/sections/Welcome /WelcomePage"),
-    loading: Loader
-});
-
-const AboutMe = LoadableVisibility({
-    loader: () => import("./Components/sections/About Me/AboutMe"),
-    loading: Loader
-});
-const Projects = LoadableVisibility({
-    loader: () => import("./Components/sections/Projects/Projects"),
-    loading: Loader
-});
-const ContactMe = LoadableVisibility({
-    loader: () => import("./Components/sections/About Me/ContactMe"),
-    loading: Loader
-});
-const Skills = LoadableVisibility({
-    loader: () => import("./Components/sections/About Me/Skills"),
-    loading: Loader
-});
-const Footer = LoadableVisibility({
-    loader: () => import("./Components/layout/Footer"),
-    loading: Loader
-});
+// const WelcomePage = LoadableVisibility({
+//     loader: () => import("./Components/sections/Welcome /WelcomePage"),
+//     loading: Loader
+// });
+//
+// const AboutMe = LoadableVisibility({
+//     loader: () => import("./Components/sections/About Me/AboutMe"),
+//     loading: Loader
+// });
+// const Projects = LoadableVisibility({
+//     loader: () => import("./Components/sections/Projects/Projects"),
+//     loading: Loader
+// });
+// const ContactMe = LoadableVisibility({
+//     loader: () => import("./Components/sections/About Me/ContactMe"),
+//     loading: Loader
+// });
+// const Skills = LoadableVisibility({
+//     loader: () => import("./Components/sections/About Me/Skills"),
+//     loading: Loader
+// });
+// const Footer = LoadableVisibility({
+//     loader: () => import("./Components/layout/Footer"),
+//     loading: Loader
+// });
 
 function App() {
 
     return (
         <Fragment>
             <Navbar/>
-            <WelcomePage/>
-            <AboutMe/>
-            <Skills/>
-            <Projects/>
-            <ContactMe/>
-            <Footer/>
+            {/*<WelcomePage/>*/}
+            {/*<AboutMe/>*/}
+            {/*<Skills/>*/}
+            {/*<Projects/>*/}
+            {/*<ContactMe/>*/}
+            {/*<Footer/>*/}
         </Fragment>
     );
 }
